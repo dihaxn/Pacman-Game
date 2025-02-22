8832ed61448b14ee4a6af77cd7f5781c9f671cf7
dihaxn <ihanlaknuka@gmail.com>
2025-02-22 11:33:05 +0530

    import images

--- Pacman/src/Main.java
+++ Pacman/src/Main.java
@@ -14,10 +14,16 @@ public class Main {
         int boardHeight = rowCount + titleSize ;
 
         JFrame frame = new JFrame("Pacman");
-        frame.setVisible(true);
+       // frame.setVisible(true);
         frame.setSize(boardWidth,boardHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        //frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
--- Pacman/src/PacMan.java
+++ Pacman/src/PacMan.java
@@ -8,12 +8,37 @@ import javax.swing.*;
 
 
 
-public class PacMan {
+public class PacMan extends JPanel {
 
-    int rowCount = 21;
-    int columnCount = 19;
-    int titleSize =32;
-    int boardWidth = columnCount + titleSize ;
-    int boardHeight = rowCount + titleSize ;
+       private int rowCount = 21;
+       private int columnCount = 19;
+       private int titleSize =32;
+       private int boardWidth = columnCount + titleSize ;
+       private int boardHeight = rowCount + titleSize ;
 
+       private Image wallImage;
+       private Image blueGhostImage;
+       private Image orangehostImage;
+       private Image pinkGhostImage;
+       private Image redGhostImage;
+
+       private Image pacmanUpImage;
+       private Image pacmanDownImage;
+       private Image pacmanLeftImage;
+       private Image pacmanRightImage;
+
+   PacMan(){
+       setPreferredSize(new Dimension(boardWidth,boardHeight));
+       setBackground(Color.BLACK);
+       wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+       blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+       orangehostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+       pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+       redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+
+       pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+       pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
+       pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+       pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+   }
 }
