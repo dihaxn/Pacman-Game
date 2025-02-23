fcda5d41ed258b46f0ffc8e076c19c8dbe69d03c
dihaxn <ihanlaknuka@gmail.com>
2025-02-23 08:53:31 +0530

    upto 25min

--- Pacman/src/PacMan.java
+++ Pacman/src/PacMan.java
@@ -10,11 +10,33 @@ import javax.swing.*;
 
 public class PacMan extends JPanel {
 
+       class Block{
+           int x;
+           int y;
+           int width;
+           int height;
+           Image image;
+
+           int startX;
+           int startY;
+
+           Block(Image image, int x, int y, int width, int height ){
+               this.image = image;
+               this.x=x;
+               this.y=y;
+               this.width = width;
+               this.height = height;
+               this.startX = x;
+               this.startY = y;
+           }
+
+       }
+
        private int rowCount = 21;
        private int columnCount = 19;
-       private int titleSize =32;
-       private int boardWidth = columnCount + titleSize ;
-       private int boardHeight = rowCount + titleSize ;
+       private int tileSize =32;
+       private int boardWidth = columnCount + tileSize ;
+       private int boardHeight = rowCount + tileSize ;
 
        private Image wallImage;
        private Image blueGhostImage;
@@ -26,6 +48,38 @@ public class PacMan extends JPanel {
        private Image pacmanDownImage;
        private Image pacmanLeftImage;
        private Image pacmanRightImage;
+
+        //X = wall, O = skip, P = pac man, ' ' = food
+        //Ghosts: b = blue, o = orange, p = pink, r = red
+        private String[] tileMap = {
+                "XXXXXXXXXXXXXXXXXXX",
+                "X        X        X",
+                "X XX XXX X XXX XX X",
+                "X                 X",
+                "X XX X XXXXX X XX X",
+                "X    X       X    X",
+                "XXXX XXXX XXXX XXXX",
+                "OOOX X       X XOOO",
+                "XXXX X XXrXX X XXXX",
+                "O       bpo       O",
+                "XXXX X XXXXX X XXXX",
+                "OOOX X       X XOOO",
+                "XXXX X XXXXX X XXXX",
+                "X        X        X",
+                "X XX XXX X XXX XX X",
+                "X  X     P     X  X",
+                "XX X X XXXXX X X XX",
+                "X    X   X   X    X",
+                "X XXXXXX X XXXXXX X",
+                "X                 X",
+                "XXXXXXXXXXXXXXXXXXX"
+        };
+
+       HashSet<Block> walls;
+       HashSet<Block> foods;
+       HashSet<Block> ghosts;
+       Block pacman;
+
 
    PacMan(){
        setPreferredSize(new Dimension(boardWidth,boardHeight));
@@ -40,5 +94,45 @@ public class PacMan extends JPanel {
        pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
        pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
        pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+
+
+   }
+
+   public void loadMap(){
+       walls = new HashSet<Block>();
+       foods = new HashSet<Block>();
+       ghosts = new HashSet<Block>();
+
+       for (int r =0 ; r< rowCount; r++){
+           for (int c =0 ; c< columnCount; c++){
+
+               String row = tileMap[r];
+               char tileMapChar = row.charAt(c);
+
+               int x = c*tileSize;
+               int y = r*tileSize;
+
+               if(tileMapChar == 'x'){
+                   Block wall = new Block(wallImage, x, y , tileSize, tileSize);
+                   walls.add(wall);
+               }
+                else if(tileMapChar == 'b'){
+                   Block ghost = new Block(blueGhostImage, x, y , tileSize, tileSize);
+                   ghosts.add(ghost);
+               }else if(tileMapChar == 'o'){
+                   Block ghost = new Block(orangehostImage, x, y , tileSize, tileSize);
+                   ghosts.add(ghost);
+               }else if(tileMapChar == 'p'){
+                   Block ghost = new Block(pinkGhostImage, x, y , tileSize, tileSize);
+                   ghosts.add(ghost);
+               }else if(tileMapChar == 'r'){
+                   Block ghost = new Block(redGhostImage, x, y , tileSize, tileSize);
+                   ghosts.add(ghost);
+               }else if(tileMapChar == 'p'){
+                   pacman = new Block(wallImage, x, y , tileSize, tileSize);
+               }
+           }
+       }
+
    }
 }
